cf368422357af26aa292bec57971dc5897bb1717
Rob <rjr211@scarletmail.rutgers.edu>
2018-09-07 18:27:53 -0400

    Initial setup

---
+++ controller/controller.js
@@ -0,0 +1,12 @@
+//dependencies
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+//require request and cheerio to scrape
+var request = require('request');
+var cheerio = require('cheerio');
+
+//Require models
+var Comment = require('../models/Comment.js');
+var Article = require('../models/Article.js');
