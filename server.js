b3870c113c23bb21d01b9fdbe59df24e395e7236
Rob <rjr211@scarletmail.rutgers.edu>
2018-09-08 22:35:50 -0400

    server setup

---
+++ server.js
@@ -0,0 +1,14 @@
+//dependencies
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var logger = require('morgan');
+
+//initialize Express app
+var express = require('express');
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
