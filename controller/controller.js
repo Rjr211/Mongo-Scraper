89e67ce278d0ccc755a22c9e24ed8cec686af401
Rob <rjr211@scarletmail.rutgers.edu>
2018-09-18 11:29:39 -0400

    Entry links and titles

--- controller/controller.js
+++ controller/controller.js
@@ -40,3 +40,36 @@ router.get('/scrape', function(req, res) {
         $('.c-entry-box--compact__title').each(function(i, element) {
             // Save an empty result object
             var result = {};
+
+                        // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this).children('a').text();
+            result.link = $(this).children('a').attr('href');
+
+            //ensures that no empty title or links are sent to mongodb
+            if(result.title !== "" && result.link !== ""){
+              //check for duplicates
+              if(titlesArray.indexOf(result.title) == -1){
+
+                // push the saved title to the array 
+                titlesArray.push(result.title);
+
+                // only add the article if is not already there
+                Article.count({ title: result.title}, function (err, test){
+                    //if the test is 0, the entry is unique and good to save
+                  if(test == 0){
+
+                    //using Article model, create new object
+                    var entry = new Article (result);
+
+                    //save entry to mongodb
+                    entry.save(function(err, doc) {
+                      if (err) {
+                        console.log(err);
+                      } else {
+                        console.log(doc);
+                      }
+                    });
+
+                  }
+            });
+        }
