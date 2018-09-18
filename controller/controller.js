a23a84944ab370256949256b246a5619d5bf8a5c
Rob <rjr211@scarletmail.rutgers.edu>
2018-09-18 11:24:04 -0400

    idex

--- controller/controller.js
+++ controller/controller.js
@@ -10,3 +10,20 @@ var cheerio = require('cheerio');
 //Require models
 var Comment = require('../models/Comment.js');
 var Article = require('../models/Article.js');
+
+//index
+router.get('/', function(req, res) {
+    res.redirect('/articles');
+});
+
+// router.get('/test-scrape', function(req, res) {
+//   request(result.link, function(error, response, html) {
+//     var $ = cheerio.load(html);
+
+//     $('.l-col__main').each(function(i, element){
+//       var result = {};
+
+//       console.log($(this).children('.c-entry-content').children('p').text());
+//     });
+//   });
+// });
--- server.js
+++ server.js
@@ -12,3 +12,27 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
+app.use(express.static(process.cwd() + '/public'));
+
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+mongoose.connect('mongodb://localhost/Mongo-Scraper');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to Mongoose!')
+});
+
+var routes = require('./controller/controller.js');
+app.use('/', routes);
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Listening on PORT ' + port);
+});
